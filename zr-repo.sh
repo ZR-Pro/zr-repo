6e20c6232ef9f4c64fe47db03112f8af47ad13c3
ZR-Pro <zakabutt@gmail.com>
2023-07-11 14:25:22 +0000

    verion1.4

--- zr-repo.sh
+++ zr-repo.sh
@@ -1,3 +1,4 @@
 echo "i am doing all my self today without having sharafat next to me loll"
 echo "somehow he is working today just the way he likes and enjoy lol"
 echo " i have done good so far and trying to understand all setps rather than remembring them"
+echo " made this update on local disc"

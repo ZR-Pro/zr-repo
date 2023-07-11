e8aff197012622fc52970bcd722ca2e63d4f560d
zaka <139076272+ZR-Pro@users.noreply.github.com>
2023-07-11 15:21:19 +0100

    Version 1.3

--- zr-repo.sh
+++ zr-repo.sh
@@ -1,3 +1,4 @@
 echo "i am doing all my self today without having sharafat next to me loll"
 echo "somehow he is working today just the way he likes and enjoy lol"
 echo " i have done good so far and trying to understand all setps rather than remembring them"
+echo " i have mad this change on GITHUB web"

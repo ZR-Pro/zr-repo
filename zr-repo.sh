a1ee134d2a2c645b5052c81f78f75b421069aed6
ZR-Pro <zakabutt@gmail.com>
2023-07-11 13:37:09 +0000

    Version1.2

--- zr-repo.sh
+++ zr-repo.sh
@@ -1,2 +1,3 @@
 echo "i am doing all my self today without having sharafat next to me loll"
 echo "somehow he is working today just the way he likes and enjoy lol"
+echo " i have done good so far and trying to understand all setps rather than remembring them"
